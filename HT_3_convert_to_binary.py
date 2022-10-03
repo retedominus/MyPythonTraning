c31c2e26072edbda1f0db63885b8fa3fb7636bca
a.dergachev <bestacc@mail.ru>
2022-10-03 20:48:57 +0300

    Create homework 3 lesson 3 convert to binary

---
+++ HT_3_convert_to_binary.py
@@ -0,0 +1,21 @@
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+# Без использования встроенной функции преобразования, без строк.
+
+
+def dec_to_bin(num):
+    ls = []
+    while num > 0:
+        prod = round(num % 2)
+        num = num // 2
+        ls.append(prod)
+    ls.reverse()
+    return ls
+
+
+def print_list(lst):
+    for i in range(len(lst)):
+        print(lst[i], end="")
+
+
+print_list(dec_to_bin(int(input("Enter the number: "))))
+
