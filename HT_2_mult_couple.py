d8f32e81e54ce2510ca49b09e7ee6d6dfc35581e
a.dergachev <bestacc@mail.ru>
2022-10-03 19:23:42 +0300

    Create homework 2 lesson 3

---
+++ HT_2_mult_couple.py
@@ -0,0 +1,27 @@
+# 2. Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+from random import sample  # не стоит ли вызов прописывать в функции?
+
+
+def create_ls(length):
+    ls = sample(range(length * 2), length)
+    return ls
+
+
+def mult_couple(lst):
+    new_ls = []
+    arg1 = (len(lst) // 2)
+    tsl = list(reversed(lst))
+    if len(lst) % 2 == 0:
+        for i in range(0, arg1):
+            new_ls.append(lst[i] * tsl[i])
+    else:
+        for i in range(0, arg1):
+            new_ls.append(lst[i] * tsl[i])
+        new_ls.append(lst[arg1])
+    return new_ls
+
+
+in_lst = create_ls(int(input("Enter the length of the list: ")))
+print(in_lst)
+print(mult_couple(in_lst))
