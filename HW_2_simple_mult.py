d345bd13eac2efe5c4efbddfa96512b051104a87
a.dergachev <bestacc@mail.ru>
2022-10-06 16:45:30 +0300

    Create homework 2 lesson 4 create simple multipliers list

---
+++ HW_2_simple_mult.py
@@ -0,0 +1,14 @@
+# 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+def smpl_mult(num):
+    ls = []
+    x = num
+    for i in range(x - 1, 0, -1):
+        if not num % i:
+            temp = num // i
+            ls.append(temp)
+            num = num // temp
+    return ls
+
+
+print(smpl_mult(int(input("Enter the number: "))))
